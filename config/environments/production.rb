7a6231ade0fe002851696b31e8e76d83261fb3fa
Yunus Emre BEYAZ <116025329+YunusEmreBEYAZ@users.noreply.github.com>
2026-06-12 10:02:12 +0200

    added local time zone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   # Full error reports are disabled.
   config.consider_all_requests_local = false
 
+  config.time_zone = "Europe/Amsterdam"
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
 
